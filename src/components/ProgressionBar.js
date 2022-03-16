0311ff21775b1ca77948f1380d8b640682091653
Liam Idrovo <liamidrovo@gmail.com>
2022-03-15 20:08:25 -0400

    Added Progression Bar Submit functionality v1

--- src/components/ProgressionBar.js
+++ src/components/ProgressionBar.js
@@ -1,17 +1,78 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 import '../styles/ProgressionBar.css';
 
-function ProgressionBar() {
+function ProgressionBar({
+  incorrectSubmissions, submitHandler, input, amountOfSlots,
+}) {
+  const [inputs, setInputs] = useState([]);
+  const [classNames, setClasses] = useState([]);
+  const [currentlySelectedSlot, selectSlot] = useState(0);
+  // TODO: set slots as state. When proceding to next progression, otherwise, ProgressionBar will
+  // not re-render
+  const slots = [];
+
+  const handleSelection = (i) => {
+    selectSlot(i);
+  };
+
+  for (let i = 0; i < amountOfSlots; i++) {
+    slots.push(
+      <div
+        className={`slot ${classNames[i]}`}
+        key={i}
+        onClick={() => handleSelection(i)}
+        onFocus={() => handleSelection(i)}
+        onKeyPress={() => handleSelection(i)}
+        tabIndex={0}
+        role="button"
+      >
+        {inputs[i]}
+      </div>,
+    );
+  }
+
+  const handleInput = (i) => {
+    const newInput = [...inputs];
+    newInput[currentlySelectedSlot] = i;
+    setInputs(newInput);
+    if (currentlySelectedSlot < amountOfSlots - 1) selectSlot((slot) => slot + 1);
+  };
+
+  useEffect(() => {
+    if (input !== '') handleInput(input);
+  }, [input]);
+
+  useEffect(() => {
+    const classes = [];
+    incorrectSubmissions.forEach((i) => {
+      classes[i] = 'incorrect';
+    });
+    setClasses(classes);
+  }, [incorrectSubmissions]);
+
   return (
     <div className="bar">
-      <Button type="round" clickHandler={() => null}>play</Button>
-      <Button type="round" clickHandler={() => null}>submit</Button>
+      <Button type="round" clickHandler={() => { }}>play</Button>
+      <div className="container">
+        {slots}
+      </div>
+      <Button type="round" clickHandler={() => submitHandler(inputs)}>submit</Button>
     </div>
   );
 }
 
-ProgressionBar.propTypes = {};
+ProgressionBar.propTypes = {
+  incorrectSubmissions: PropTypes.arrayOf(PropTypes.number),
+  submitHandler: PropTypes.func.isRequired,
+  input: PropTypes.string,
+  amountOfSlots: PropTypes.number.isRequired,
+};
+
+ProgressionBar.defaultProps = {
+  incorrectSubmissions: [],
+  input: '',
+};
 
 export default ProgressionBar;
--- src/components/Training.js
+++ src/components/Training.js
@@ -1,24 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CircleSelection from './CircleSelection';
-import { Scale } from '../lib/progressions';
-import InputDisplay from './InputDisplay';
+import { Scale, getDiatonicChords } from '../lib/progressions';
 import Header from './Header';
 import ProgressionBar from './ProgressionBar';
 
 const scale = Scale.MAJOR;
 
+const diatonicMajorScaleChords = getDiatonicChords(scale);
+
+const progression = [diatonicMajorScaleChords[0],
+  diatonicMajorScaleChords[1],
+  diatonicMajorScaleChords[2],
+  diatonicMajorScaleChords[3]];
+
 export default function Training() {
+  // eslint-disable-next-line no-unused-vars
   const [input, setInput] = useState('');
+  const [incorrectChords, setIncorrectChords] = useState([]);
 
   const inputHandler = (newInput) => {
-    setInput((x) => `${x} - ${newInput}`);
+    setInput(newInput);
   };
+
+  // eslint-disable-next-line no-unused-vars
+  const submitHandler = (inputtedChords) => {
+    console.log('checking submission ...');
+    const incorrectIndices = [];
+    for (let i = 0; i < progression.length; i++) {
+      if (inputtedChords[i] !== progression[i]) {
+        incorrectIndices.push(i);
+      }
+    }
+    setIncorrectChords(incorrectIndices);
+  };
+
+  useEffect(() => {
+    // Resets input to empty string to trigger prop change  in case same chord is inputted.
+    // Otherwise, ProgressionBar won't consider consecutive clicks of the same chord.
+    setInput('');
+  }, [input]);
 
   return (
     <div>
       <Header>Progression Training</Header>
-      <ProgressionBar />
-      <InputDisplay input={input} />
+      <ProgressionBar
+        amountOfSlots={progression.length}
+        submitHandler={submitHandler}
+        input={input}
+        incorrectSubmissions={incorrectChords}
+      />
       <CircleSelection scale={scale} sendInput={inputHandler} />
     </div>
   );
