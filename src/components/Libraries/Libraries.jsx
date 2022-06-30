fac204bb1e988d6f30b3c016c5ffccf6123bb555
Liam Idrovo <liamidrovo@gmail.com>
2022-06-30 19:00:35 -0400

    Library tabs visual functionality implemented

--- src/components/Libraries/Libraries.jsx
+++ src/components/Libraries/Libraries.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LibrarySearch from '../LibrarySearch/LibrarySearch';
 import styles from './Libraries.module.css';
 
 function Libraries() {
+  const [library, setLibrary] = useState('public');
+
+  console.log(library);
+
+  const handleTabClick = (lib, e) => {
+    if (e.type === 'keydown' && e.key !== 'Enter') {
+      return;
+    }
+    setLibrary(lib);
+  };
+
   return (
     <div className={styles.container}>
       <LibrarySearch />
+      <div className={styles.libraryTabs}>
+        <div
+          className={`${styles.tab} ${library === 'personal' ? styles.activeTab : null}`}
+          onClick={(e) => handleTabClick('personal', e)}
+          onKeyDown={(e) => handleTabClick('personal', e)}
+          tabIndex={0}
+          role="button"
+        >
+          Personal
+
+        </div>
+        <div
+          className={`${styles.tab} ${library === 'public' ? styles.activeTab : null}`}
+          onClick={(e) => handleTabClick('public', e)}
+          onKeyDown={(e) => handleTabClick('public', e)}
+          tabIndex={0}
+          role="button"
+        >
+          Public
+
+        </div>
+      </div>
     </div>
   );
 }
