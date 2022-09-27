b3229334e22275ed31b4813c261b5f01c874376a
Liam Idrovo <liamidrovo@gmail.com>
2022-09-27 17:24:46 -0400

    changed base url

--- src/utils/backend.js
+++ src/utils/backend.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
   let url;
   switch (process.env.NODE_ENV) {
     case 'production':
-      url = 'https://184.72.155.177';
+      url = 'https://34.229.88.194';
       break;
     case 'development':
     default:
